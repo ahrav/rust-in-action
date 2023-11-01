c9254c391af2d9356fc094d8e57c6968c7b7f570
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2023-11-01 07:48:08 -0700

    add memory key/val implementation

--- src/actionkv/src/akv_mem.rs
+++ src/actionkv/src/akv_mem.rs
@@ -1,3 +1,52 @@
-fn main() {
+use libactionkv::ActionKV;
 
+#[cfg(target_os = "windows")]
+const USAGE: &str = "
+Usage:
+    akv_mem.exe FILE get KEY
+    akv_mem.exe FILE delete KEY
+    akv_mem.exe FILE insert KEY VALUE
+    akv_mem.exe FILE update KEY VALUE
+";
+
+#[cfg(not(target_os = "windows"))]
+const USAGE: &str = "
+Usage:
+    akv_mem FILE get KEY
+    akv_mem FILE delete KEY
+    akv_mem FILE insert KEY VALUE
+    akv_mem FILE update KEY VALUE
+";
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let filename = args.get(1).expect(&USAGE);
+    let action = args.get(2).expect(&USAGE).as_ref();
+    let key = args.get(3).expect(&USAGE).as_ref();
+    let value = args.get(4).expect(&USAGE).as_ref();
+
+    let path = std::path::Path::new(&filename);
+    let mut akv = ActionKV::open(path).expect("Failed to open ActionKV");
+    akv.load().expect("Failed to load ActionKV");
+
+    match action {
+        "get" => {
+            match akv.get(key).unwrap() {
+                Some(value) => println!("{}", String::from_utf8(value).unwrap()),
+                None => eprintln!("Key not found"),
+            }
+        }
+        "delete" => {
+            akv.delete(key).unwrap();
+        }
+        "insert" => {
+            let val = value.expect(&USAGE).as_ref();
+            akv.insert(key, val).unwrap();
+        }
+        "update" => {
+            let val = value.expect(&USAGE).as_ref();
+            akv.update(key, val).unwrap();
+        }
+        _ => eprintln!("Unknown action"),
+    }
 }
--- src/actionkv/src/lib.rs
+++ src/actionkv/src/lib.rs
@@ -1,7 +1,8 @@
 use std::collections::HashMap;
 use std::fs::{File, OpenOptions};
 use std::io;
-use std::io::Read;
+use std::io::prelude::*;
+use std::io::{BufReader, Read, SeekFrom};
 use std::path::Path;
 
 use crc32fast::Hasher;
@@ -59,6 +60,121 @@ impl ActionKV {
         let key = data;
 
         Ok(KeyValuePair { key, value })
+    }
+
+    pub fn seek_to_end(&mut self) -> io::Result<u64> {
+        self.f.seek(io::SeekFrom::End(0))
+    }
+
+    pub fn load(&mut self) -> io::Result<()> {
+        let mut f = BufReader::new(&self.f);
+
+        loop {
+            let curr_pos = f.seek(io::SeekFrom::Current(0))?;
+
+            let maybe_kv = ActionKV::process_record(&mut f);
+            let kv = match maybe_kv {
+                Ok(kv) => kv,
+                Err(e) => {
+                    match e.kind() {
+                        io::ErrorKind::UnexpectedEof => break,
+                        _ => return Err(e),
+                    }
+                }
+            };
+            self.idx.insert(kv.key, curr_pos);
+        }
+       Ok(())
+    }
+
+    pub fn get(&mut self, key: &ByteStr) -> io::Result<Option<ByteString>> {
+        let pos = match self.idx.get(key) {
+            Some(pos) => *pos,
+            None => return Ok(None),
+        };
+
+        let kv = self.get_at(pos)?;
+        Ok(Some(kv.value))
+    }
+
+    pub fn get_at(&mut self, pos: u64) -> io::Result<KeyValuePair> {
+        let mut f = BufReader::new(&self.f);
+        f.seek(SeekFrom::Start(pos))?;
+        Ok(ActionKV::process_record(&mut f)?)
+    }
+
+    pub fn find(&mut self, key: &ByteStr) -> io::Result<Option<(u64, ByteString)>> {
+        let mut f = BufReader::new(&self.f);
+        let mut found: Option<(u64, ByteString)> = None;
+
+        loop {
+            let curr_pos = f.seek(io::SeekFrom::Current(0))?;
+
+            let maybe_kv = ActionKV::process_record(&mut f);
+            let kv = match maybe_kv {
+                Ok(kv) => kv,
+                Err(e) => {
+                    match e.kind() {
+                        io::ErrorKind::UnexpectedEof => break,
+                        _ => return Err(e),
+                    }
+                }
+            };
+
+            if kv.key == key {
+                found = Some((curr_pos, kv.value));
+            }
+        }
+        Ok(found)
+    }
+
+    pub fn insert(&mut self, key: &ByteStr, val: &ByteStr) -> io::Result<()> {
+        let pos = self.insert_but_ignore_index(key, val)?;
+        self.idx.insert(key.to_vec(), pos);
+        Ok(())
+    }
+
+    pub fn insert_but_ignore_index(&mut self, key: &ByteStr, val: &ByteStr) -> io::Result<u64> {
+        let mut f = BufReader::new(&self.f);
+
+        let key_len = key.len();
+        let val_len = val.len();
+        let mut tmp = ByteString::with_capacity(key_len + val_len);
+        for b in key {
+            tmp.push(*b);
+        }
+
+        for b in val {
+            tmp.push(*b);
+        }
+
+        let mut hasher = Hasher::new();
+        hasher.update(b"hello world");
+        let checksum = hasher.finalize();
+
+        let next_byte = SeekFrom::End(0);
+        let pos = f.seek(SeekFrom::Current(0))?;
+        f.seek(next_byte)?;
+
+        f.write_u32::<LittleEndian>(checksum)?;
+        f.write_u32::<LittleEndian>(key.len() as u32)?;
+        f.write_u32::<LittleEndian>(val.len() as u32)?;
+        f.write_all(&tmp)?;
+        Ok(pos)
+    }
+
+    #[inline]
+    pub fn update(
+        &mut self,
+        key: &ByteStr,
+        value: &ByteStr,
+    ) -> io::Result<()> {
+        self.insert(key, value)
+    }
+
+    #[inline]
+    pub fn delete(&mut self, key: &ByteStr) -> io::Result<()> {
+        self.insert(key, b"")
     }
 }
 
